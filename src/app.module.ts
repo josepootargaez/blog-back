2fee58e6795aa38ada45d698f11b6eb97c0b9d68
jose.poot <jose-9803@hotmail.com>
2023-12-21 21:36:00 -0500

    change DB mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './modules/blog/blog.module';
+require("dotenv").config();
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI),
     BlogModule
   ],
   controllers: [],
